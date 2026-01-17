7e0aeb19e194d7606af65f4ebb4606ae9ac2d46f
shwady <ahmedelshwady2004@gmail.com>
2026-01-17 02:08:12 +0200

    kinda finished the reading taxi records notebook and turned it into script

---
+++ reading_taxi_records.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd 
+
+
+# In[2]:
+
+
+prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
+url = f'{prefix}yellow_tripdata_2020-03.csv.gz'
+
+
+# In[3]:
+
+
+dtype = {
+    "VendorID": "Int64",
+    "passenger_count": "Int64",
+    "trip_distance": "float64",
+    "RatecodeID": "Int64",
+    "store_and_fwd_flag": "string",
+    "PULocationID": "Int64",
+    "DOLocationID": "Int64",
+    "payment_type": "Int64",
+    "fare_amount": "float64",
+    "extra": "float64",
+    "mta_tax": "float64",
+    "tip_amount": "float64",
+    "tolls_amount": "float64",
+    "improvement_surcharge": "float64",
+    "total_amount": "float64",
+    "congestion_surcharge": "float64"
+}
+
+parse_dates = [
+    "tpep_pickup_datetime",
+    "tpep_dropoff_datetime"
+]
+
+
+# In[4]:
+
+
+from sqlalchemy import create_engine
+engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
+
+
+# In[5]:
+
+
+df_iter = pd.read_csv(
+    url,
+    dtype=dtype,
+    parse_dates=parse_dates,
+    iterator=True,
+    chunksize=50000
+)
+
+
+# In[6]:
+
+
+from tqdm.auto import tqdm
+for df_chunk in tqdm(df_iter):
+    df_chunk.to_sql(name= "yellow_taxi_data", con=engine, if_exists="append")
+
+
+# In[ ]:
+
+
+
+
